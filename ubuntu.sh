1d3c62370a43e5eb74b139950e29572c8b2a8021
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-09-22 22:14:37 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,7 +3,7 @@
 #解决中文乱码
 echo "以下命令需要超级用户权限。"
 sudo su
-apt-get install language-pack-zh-hans language-pack-zh-hans-base language-pack-gnome-zh-hans language-pack-gnome-zh-hans-base
+apt-get install `check-language-support -l zh-hans`
 echo -e "LANG=\"zh_CN.UTF-8\"\nLANGUAGE=\"zh_CN:zh:en_US:en\"" >> "/etc/environment"
 echo -e "en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8\nzh_CN.GBK GBK" >> "/var/lib/locales/supported.d/local"
 locale-gen

e404facfe74c1d161be4de5f5a2d7f7b98c31e15
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-09-22 22:00:41 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 #解决中文乱码
-sudo echo "LANG=\"zh_CN.UTF-8\"\nLANGUAGE=\"zh_CN:zh:en_US:en\"" >> "/etc/environment"
+sudo echo -e "LANG=\"zh_CN.UTF-8\"\nLANGUAGE=\"zh_CN:zh:en_US:en\"" >> "/etc/environment"

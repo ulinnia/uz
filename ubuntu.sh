a261eee03921e6ce700f24a56f13443b3ddc81fc
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-09-22 22:51:28 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -8,4 +8,6 @@ echo -e "LANG=\"zh_CN.UTF-8\"\nLANGUAGE=\"zh_CN:zh:en_US:en\"" >> "/etc/environm
 echo -e "en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8\nzh_CN.GBK GBK" >> "/var/lib/locales/supported.d/local"
 locale-gen
 #安装常用软件
+apt update
+apt upgrade
 apt install vim fcitx fcitx-rime

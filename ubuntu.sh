6182052d9eff411ceef89a3685a26808ff6cc393
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-09-23 21:40:34 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -20,6 +20,7 @@ sed -i 's/XKBOPTIONS=""/XKBOPTIONS="ctrl:nocaps"/' /etc/default/keyboard
 #安装小鹤音形
 exit
 if [ -e "~/下载" ]; then dl="下载"; else dl="Download"; fi
+#到http://flypy.ys168.com/ 小鹤音形挂接第三方 小鹤音形Rime平台鼠须管for macOS.zip
 link=https://github.com/Caffreyfans/flypy-install/raw/master/rime.zip
 wget ${link} -O ~/${dl}/hrime.zip
 7z x ~/${dl}/hrime.zip -o~/${dl}

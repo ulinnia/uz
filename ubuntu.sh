57a8a07112eb8771e5c5c2d22d57e1de69d99243
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-09-23 21:36:12 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -3,16 +3,16 @@
 #解决中文乱码
 echo "以下命令需要超级用户权限。"
 sudo su
-apt-get install `check-language-support -l zh-hans`
+apt install `check-language-support -l zh-hans` -y
 echo -e "LANG=\"zh_CN.UTF-8\"\nLANGUAGE=\"zh_CN:zh:en_US:en\"" >> "/etc/environment"
 echo -e "en_US.UTF-8 UTF-8\nzh_CN.UTF-8 UTF-8\nzh_CN.GBK GBK" >> "/var/lib/locales/supported.d/local"
 locale-gen
 
 #安装常用软件
-apt update
-apt upgrade
-apt install vim fcitx fcitx-rime wget curl p7zip
-apt autoremove
+apt update -y
+apt upgrade -y
+apt install vim fcitx fcitx-rime wget curl p7zip -y
+apt autoremove -y
 
 #CAPS改为CTRL
 sed -i 's/XKBOPTIONS=""/XKBOPTIONS="ctrl:nocaps"/' /etc/default/keyboard

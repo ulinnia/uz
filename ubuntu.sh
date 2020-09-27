7a5713205fca2a74088bd7803a862044451a1580
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-09-27 19:11:00 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -21,6 +21,13 @@ fcitx-remote -r
 echo "请在下个视窗切换成fcitx, enter" choice
 im-config
 
+#GNOME扩展启用
+echo "进入扩展页面启用 Touchpad Indicator 和 User Themes 扩展，enter" choice
+firefix https://extensions.gnome.org/extension/131/touchpad-indicator/
+firefox https://extensions.gnome.org/extension/19/user-themes/
+echo "打开 GNOME 调整工具（优化），进入“外观”部分，就可以看到 shell 主题的选项，现在只需要把它启用就可以了。" choice
+
+
 #用control+space来切换到rime
 read -p "重启后，用control+space来切换到rime，请按任意键以重启，(n/N)停止:" choice
 if [[ $choice = "n" ]]||[[ $choice = "N" ]]; then

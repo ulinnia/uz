dd66dff72a3dfb7e571ff3bf1b4ec238962ff3a4
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-09-28 21:18:33 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -57,7 +57,7 @@ if [[ $choice = "y" ]]||[[ $choice = "Y" ]]; then
  sudo add-apt-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ ${ver} main'
  sudo apt update -y
  sudo apt install --install-recommends winehq-staging -y
- sudo install -y libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386
+ sudo apt install -y libgnutls30:i386 libldap-2.4-2:i386 libgpg-error0:i386 libxml2:i386 libasound2-plugins:i386 libsdl2-2.0-0:i386 libfreetype6:i386 libdbus-1-3:i386 libsqlite3-0:i386
  read -p "打开lutris，选项，首选项，系统选项，显示高级选项，在命令前缀输入gamemoderun，保存。enter"
 fi
 

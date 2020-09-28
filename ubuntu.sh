7ba7b81d2478d5f2261d7e683e2820147789b34b
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-09-28 16:38:53 +0800

    Update ubuntu.sh

--- ubuntu.sh
+++ ubuntu.sh
@@ -30,9 +30,9 @@ read -p "打开GNOME调整工具（优化），进入“外观”部分，就可
 #游戏
 read -p "有玩游戏的需求吗，按Y确认" choice
 if [[ $choice = "y" ]]||[[ $choice = "Y" ]]; then
-    sudo add-apt-repository ppa:lutris-team/lutris
-    sudo apt update
-    sudo apt install lutris steam
+    sudo add-apt-repository -y ppa:lutris-team/lutris
+    sudo apt update -y
+    sudo apt install -y lutris steam
 fi
 
 #用control+space来切换到rime

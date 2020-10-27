35b9d388ce09a07b7005106a4c5c76eb38f8db05
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-10-27 20:07:07 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -42,5 +42,7 @@ yay --aururl "https://aur.tuna.tsinghua.edu.cn" --save
 ​7z x flypy.zip
 ​cp -Rf ​"​小鹤音形Rime平台鼠须管for macOS/rime​"​ ​~​/.config/fcitx
 ​rm -rf ​"​小鹤音形Rime平台鼠须管for macOS​"​ flypy.zip
+rm ~/.config/fcitx/rime/default.yaml && fcitx-remote -r
+
 
 

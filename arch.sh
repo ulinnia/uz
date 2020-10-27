0e4d8f40e2421a980ac917586b7e446a987291af
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-10-27 13:48:01 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-#安装声卡、显卡、xorg、触摸板、字体及常用程序
-sudo pacman -S alsa-utils pulseaudio-alsa xf86-video-vesa xorg xorg-xinit xf86-input-libinput noto-fonts-cjk ttf-ubuntu-font-family curl git p7zip tree vlc wget zsh
+#安装声卡、显卡、xorg、触摸板、字体、fcitx及常用程序
+sudo pacman -S alsa-utils pulseaudio-alsa xf86-video-vesa xorg xorg-xinit xf86-input-libinput noto-fonts-cjk ttf-ubuntu-font-family fcitx fcitx-im fcitx-rime fcitx-config curl git p7zip tree vlc wget zsh
 
 cp /etc/X11/xinit/xinitrc ~/.xinitrc

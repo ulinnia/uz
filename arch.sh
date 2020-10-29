a0cbb400569b782614525a3436f56d6be12248e1
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-10-29 14:33:42 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -31,7 +31,7 @@ wget ${link}conf/vimrc -O ~/.vimrc
 wget ${link}conf/zshrc -O ~/.zshrc
 
 #自启动
-sudo systemctl enable {tlp,systemd-swap,NetworkManager,NetworkManager-dispatcher}
+sudo systemctl enable {tlp,systemd-swap,NetworkManager}
 sudo systemctl disable {dhcpcd,netctl}
 sudo systemctl mask {systemd-rfkill.service,systemd-rfkill.socket}
 

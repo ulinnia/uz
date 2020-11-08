a276af4bdab2e89c92ec37f4b0e54e86cc57067e
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-11-08 20:31:02 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -86,4 +86,7 @@ sudo pacman -Syy --noconfirm ttf-liberation wqy-zenhei nvidia lib32-nvidia-libgl
 # 下载 vim-plug
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
+# 安装 SpaceVim
+curl -sLf https://spacevim.org/cn/install.sh | bash
+
 

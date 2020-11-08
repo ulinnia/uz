3986d4fa4e9a35832ab30d96bd49b357d70b88e5
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-11-08 19:37:20 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -83,4 +83,7 @@ sudo grub-mkconfig -o /boot/grub/grub.cfg
 sudo sed -i "/\[multilib\]/,+1s/#//g" /etc/pacman.conf
 sudo pacman -Syy --noconfirm ttf-liberation wqy-zenhei nvidia lib32-nvidia-libgl steam
 
+# 下载 vim-plug
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
 

349f2eb24e44ff9b39f93e6fe6e658547597bdc2
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-11-09 18:05:14 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -29,7 +29,7 @@ yay -S --noconfirm oh-my-zsh-git
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 # 安装 nodejs
-curl -sL install-node.now.sh/lts | bash
+echo -e "yes\n" | bash $(curl -sL install-node.now.sh/lts)
 
 # 配置 grub、tlp、init、i3、urxvt、nvim、zsh、CAPS CTRL 对调、壁纸
 link=https://raw.githubusercontent.com/rraayy246/UZ/master/

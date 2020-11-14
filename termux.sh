cc372f77a610c9fb1121b50c62859b11c5f2b459
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-11-14 15:03:02 +0800

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,44 +1,43 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-#连接内部存储。
+# 连接内部存储。
 termux-setup-storage
 
-# ======= 安装 Arch 系统 =======
-pkg install -y proot git bsdtar
-cd ~
-git clone https://github.com/sdrausty/TermuxArch
-bash TermuxArch/setupTermuxArch.sh
-pacman -Syy
+# 安装软件
+pkg install -y curl git man nvim tree wget zsh
 
-pacman -S --noconfirm curl man nvim tree wget zsh
-
-#安装oh-my-zsh。
+# 安装 oh-my-zsh。
 git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh --depth 1 #浅克隆
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-#换成amuse主题。
+# 换成 amuse 主题。
 sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="amuse"/' ~/.zshrc
-#开启zsh自动更新。
+# 开启 zsh 自动更新。
 if [ "$(grep "DISABLE_UPDATE_PROMPT=true" ~/.zshrc)" == "" ]; then
  echo "DISABLE_UPDATE_PROMPT=true" >> "~/.zshrc"
 fi
 
-#安装zsh语法高亮。
+# ======= 配置文件 =======
+# 用变数代替我的 github 仓库网址
+link=https://raw.githubusercontent.com/rraayy246/UZ/master/
+# 下载配置文件
+mkdir ~/.config/nvim; wget -q ${link}P/vim -O ~/.config/nvim/init.vim
+wget -q ${link}P/zshrc -O ~/.zshrc
+
+# 安装 zsh 语法高亮。
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.zsh-syntax-highlighting --depth 1
 if [ "$(grep "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" ~/.zshrc)" == "" ]; then
  echo "source ~/.zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> "~/.zshrc"
 fi
 
-#设zsh为默认shell
+# 设 zsh 为默认 shell
 chsh -s zsh
 
-#vim设定：显示行号，语法高亮，大小写混搜。
-
-#软连接aidn。
+# 软连接 aidn。
 if [ -e "~/storage/shared/A/Y/aidn" ]; then ln -s ~/storage/shared/A/Y/aidn ~/aidn; fi
 #安装Ubuntu字体。
 curl -fsLo ~/.termux/font.ttf --create-dirs https://github.com/powerline/fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline.ttf
 
-#应用设定。
+# 应用设定。
 termux-reload-settings
 
 echo "完成！请重启Termux。"

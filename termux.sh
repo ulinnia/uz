7e4138f65cd6cec3361070e49971a5b26699196d
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-11-28 12:20:33 +0800

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -7,7 +7,7 @@ termux-setup-storage
 pkg install -y curl git man neovim tree wget zsh
 
 # 安装 oh-my-zsh。
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 
 # ======= 配置文件 =======
 # 用变数代替我的 github 仓库网址

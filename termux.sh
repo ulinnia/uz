d5d8a6ab71fcab5f35c7619466c9e20ead379b6b
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-11-28 15:09:06 +0800

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -17,8 +17,8 @@ mkdir -p ~/.config/nvim
 wget -nv ${link}P/vim.vim -O ~/.config/nvim/init.vim
 wget -nv ${link}P/zshrc -O ~/.zshrc
 
-# 注释 nvim 配置 plug 插件
-sed -i "/^call plug#begin/,/^call plug#end/s/^[^#]/#&/" ~/.config/nvim/init.vim
+# 注释 nvim plug 配置
+sed -i "/^call plug#begin/,$s/^[^#]/#&/" ~/.config/nvim/init.vim
 
 # 设 zsh 为默认 shell
 chsh -s zsh

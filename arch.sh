82f97624d61fe6f255dd2e81f95ba3d227df2436
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-11-28 14:32:03 +0800

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -12,8 +12,8 @@ gpu=xf86-video-nouveau; fi
 
 # ======= 下载软件 =======
 # 增加 multilib 源
-sudo sed -i "/^\[multilib\]/,+1s/#//g" /etc/pacman.conf
-sudo sed -i "/^#Color/s/#//" /etc/pacman.conf
+sudo sed -i "/^\[multilib\]/,+1s/^#//g" /etc/pacman.conf
+sudo sed -i "/^#Color/s/^#//" /etc/pacman.conf
 # 更新系统并安装 btrfs 管理、网络管理器、tlp
 echo -e "\n" | sudo pacman -Syu btrfs-progs networkmanager tlp tlp-rdw
 # 声卡、触摸板、显卡驱动

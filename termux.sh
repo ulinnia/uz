33d25cc563eae07a557a8576fb156b5089eba660
rraayy246 <30580339+rraayy246@users.noreply.github.com>
2020-12-14 01:03:17 +0800

    Update termux.sh

--- termux.sh
+++ termux.sh
@@ -1,39 +1,71 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # 连接内部存储。
-termux-setup-storage
+nbci_lj() {
+ termux-setup-storage
+}
 
 # 安装软件
-pkg install -y curl git man neovim tree wget zsh
+rj_av() {
+ pkg install -y curl git man neovim tree wget zsh
+ # 安装 oh-my-zsh。
+ git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh --depth 1
+}
 
-# 安装 oh-my-zsh。
-git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh --depth 1
-
-# ======= 配置文件 =======
-# 用变数代替我的 github 仓库网址
-link=https://raw.githubusercontent.com/rraayy246/UZ/master/
 # 下载配置文件
-mkdir -p ~/.config/nvim
-wget -nv ${link}P/vim.vim -O ~/.config/nvim/init.vim
-wget -nv ${link}P/zshrc -O ~/.zshrc
+pvwj_xz() {
+ # 用变数代替我的 github 仓库网址
+ ck_wv=https://raw.githubusercontent.com/rraayy246/UZ/master/
+ # 下载配置文件
+ mkdir -p ~/.config/nvim
+ wget -nv ${ck_wv}P/vim.vim -O ~/.config/nvim/init.vim
+ wget -nv ${ck_wv}P/zshrc -O ~/.zshrc
+ # 下载 Ubuntu 字体
+ curl -fsLo ~/.termux/font.ttf --create-dirs https://github.com/powerline/fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline.ttf
+}
 
-# 修改安装路径
-sed -i "/^ZSH=/s/.*/export ZSH=~\/.oh-my-zsh/" ~/.zshrc
+# 设置 zsh
+zsh_uv() {
+ # 修改安装路径
+ sed -i "/^ZSH=/s/.*/export ZSH=~\/.oh-my-zsh/" ~/.zshrc
+ # 设 zsh 为默认 shell
+ chsh -s zsh
+}
 
-# 设 zsh 为默认 shell
-chsh -s zsh
-
-# nvim 注释 plug 配置
-sed -i '/^call plug#begin/,$s/^[^"]/"&/' ~/.config/nvim/init.vim
+# 设置 vim
+vim_uv() {
+ # nvim 注释 plug 配置
+ sed -i '/^call plug#begin/,$s/^[^"]/"&/' ~/.config/nvim/init.vim
+}
 
 # 软连接 aidn。
-if [ -e "$HOME/storage/shared/A/B/aidn" ]; then ln -s ~/storage/shared/A/B/aidn ~/aidn; fi
-#安装Ubuntu字体。
-curl -fsLo ~/.termux/font.ttf --create-dirs https://github.com/powerline/fonts/raw/master/UbuntuMono/Ubuntu%20Mono%20derivative%20Powerline.ttf
+aidn_lj() {
+ if [ -e "$HOME/storage/shared/A/B/aidn" ]; then
+  ln -s ~/storage/shared/A/B/aidn ~/aidn
+ fi
+}
 
 # 应用设定。
-termux-reload-settings
+ud_yy() {
+ termux-reload-settings
+}
 
-echo "完成！请重启Termux。"
+# 文字提醒
+wztx() {
+ echo "完成！请重启Termux。"
+ exit
+}
 
-exit
+# ======= 主程序 =======
+vix_yx() {
+ nbci_lj
+ rj_av
+ pvwj_xz
+ zsh_uv
+ vim_uv
+ aidn_lj
+ ud_yy
+ wztx
+}
+
+vix_yx

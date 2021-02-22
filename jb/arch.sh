37e4ea7bdbeccc5f6fcbc2896238168869ceb832
ray <rraayy246@gmail.com>
2021-02-22 19:33:58 +0800

    mv

--- jb/arch.sh
+++ jb/arch.sh
@@ -114,6 +114,7 @@ xhyx_av() {
     cd
     # 解压配置包
     7z x ${pvwj}flypy.7z
+    mkdir -p ~/.local/share/fcitx5/rime
     mv -f ~/rime ~/.local/share/fcitx5/rime
     # 重新加载 fcitx 配置
     fcitx5-remote -r

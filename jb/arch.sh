ccfef295000a7f4c0533c527a9ff7d15bffa9742
ray <rraayy246@gmail.com>
2021-02-24 12:37:53 +0800

    hjbl

--- jb/arch.sh
+++ jb/arch.sh
@@ -91,6 +91,38 @@ zsh_uv() {
     # 安装 zlua
     wget -nv https://raw.githubusercontent.com/skywind3000/z.lua/master/z.lua -O ~/.config/fish/conf.d/z.lua
     echo "source (lua ~/.config/fish/conf.d/z.lua --init fish | psub)" > ~/.config/fish/conf.d/z.fish
+
+    fish_hjbl
+}
+
+# fish 设置环境变量
+fish_hjbl() {
+
+    # 移除欢迎语
+    set -U fish_greeting ""
+
+    # 语言设置
+    set -Ux LANG zh_CN.UTF-8
+    set -Ux LANGUAGE zh_CN:en_US
+    set -Ux LC_CTYPE en_US.UTF-8
+
+    # 设定输入法
+    set -Ux GTK_IM_MODULE fcitx
+    set -Ux QT_IM_MODULE fcitx
+    set -Ux XMODIFIERS @im=fcitx
+    set -Ux SDL_IM_MODULE fcitx
+
+    # nnn 书签，选择，插件，缓存
+    set -Ux NNN_BMS 'a:~/a;x:~/xz;j:~;g:~/gz'
+    set -Ux NNN_SEL '/tmp/.sel'
+    set -Ux NNN_PLUG ''
+    set -Ux NNN_FIFO '/tmp/nnn.fifo'
+
+    # 默认编辑器
+    set -Ux EDITOR nvim
+
+    # 控制键替换大写锁定键
+    set -Ux XKB_DEFAULT_OPTIONS ctrl:nocaps
 }
 
 # 下载配置文件
@@ -103,7 +135,6 @@ pvwj_xz() {
     pvwj=~/a/uz/pv/
     sudo ln -f ${pvwj}grub /etc/default/grub
     sudo ln -f ${pvwj}tlp /etc/tlp.conf
-    ln -f ${pvwj}hjbl ~/.config/fish/fish_variables
     ln -f ${pvwj}fish.fish ~/.config/fish/config.fish
     ln -f ${pvwj}sway ~/.config/sway/config
     ln -f ${pvwj}vtl.sh ~/.config/sway/vtl.sh

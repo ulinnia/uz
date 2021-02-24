812fe324f7eb42885decc869580817de83cddb54
ray <rraayy246@gmail.com>
2021-02-24 20:36:19 +0800

    ter

--- jb/termux.sh
+++ jb/termux.sh
@@ -78,6 +78,28 @@ fish_hjbl() {
 
         # 控制键替换大写锁定键
         set -Ux XKB_DEFAULT_OPTIONS ctrl:nocaps
+
+        # fish 提示符
+        set -U __fish_git_prompt_show_informative_status 1
+        set -U __fish_git_prompt_hide_untrackedfiles 1
+
+        set -U __fish_git_prompt_color_branch magenta --bold
+        set -U __fish_git_prompt_showupstream \"informative\"
+        set -U __fish_git_prompt_char_upstream_ahead \"↑\"
+        set -U __fish_git_prompt_char_upstream_behind \"↓\"
+        set -U __fish_git_prompt_char_upstream_prefix \"\"
+
+        set -U __fish_git_prompt_char_stagedstate \"+\"
+        set -U __fish_git_prompt_char_dirtystate \"*\"
+        set -U __fish_git_prompt_char_untrackedfiles \"…\"
+        set -U __fish_git_prompt_char_conflictedstate \"\#\"
+        set -U __fish_git_prompt_char_cleanstate \"√\"
+
+        set -U __fish_git_prompt_color_dirtystate blue
+        set -U __fish_git_prompt_color_stagedstate yellow
+        set -U __fish_git_prompt_color_invalidstate red
+        set -U __fish_git_prompt_color_untrackedfiles $fish_color_normal
+        set -U __fish_git_prompt_color_cleanstate green --bold
     "
 }
 

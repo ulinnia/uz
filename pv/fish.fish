7841d9370ba5a7b281897d6e807bd1ceb665e981
ray <rraayy246@gmail.com>
2021-02-27 20:20:48 +0800

    sway

--- pv/fish.fish
+++ pv/fish.fish
@@ -1,7 +1,7 @@
 # ~/.config/fish/config.fish
 
 # 如果没有图形显示，则启动 sway
-if test -z $DISPLAY && test "(tty)" = "/dev/tty1"
+if test -z $DISPLAY && test (tty) = "/dev/tty1"
     exec sway
 
 # 如果是交互状态，则启动 starship

58d120a332769f8124a7fd40da02619f1ec19335
ray <rraayy246@gmail.com>
2021-02-28 01:58:01 +0800

    dr

--- pv/fish.fish
+++ pv/fish.fish
@@ -1,10 +1,8 @@
 # ~/.config/fish/config.fish
 
 # 如果登入没有图形显示，则启动 sway
-if status is-login
-    if test -z "$DISPLAY" -a (tty) = "/dev/tty1"
+if status is-login && test -z "$DISPLAY" -a (tty) = "/dev/tty1"
         exec sway
-    end
 
 # 如果是交互状态，则启动 starship
 else if status is-interactive

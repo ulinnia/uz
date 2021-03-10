a08fa9f65bb2cfeea498edfb53c948810ef203fe
ray <rraayy246@gmail.com>
2021-03-10 17:52:01 +0800

    a

--- jb/arch.fish
+++ jb/arch.fish
@@ -198,7 +198,7 @@ end
 # 设置 vim
 function vim_uv
     # 安装 vim-plug
-    sh -c 'curl -fLo '{$XDG_DATA_HOME:-$HOME/.local/share}'/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+    curl -fLo $HOME/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
     # 插件下载
     nvim +PlugInstall +qall
 end

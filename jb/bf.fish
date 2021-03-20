211f4deab12babc0a480aafa7b277f98bd15f4b4
ray <rraayy246@gmail.com>
2021-03-21 03:20:19 +0800

    a

--- jb/bf.fish
+++ jb/bf.fish
@@ -13,4 +13,4 @@ set root (string split ' ' (string match '* /' (df)))
 sudo umount /mnt
 sudo mount $root[1] /mnt
 sudo btrfs subvolume delete -c /mnt/b
-sudo btrfs subvolume snapshot -r /mnt/a /mnt/b
+sudo btrfs subvolume snapshot /mnt/a /mnt/b

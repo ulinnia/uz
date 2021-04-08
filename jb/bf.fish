c39b427aa55bef9035a6120f902665fe477257e0
ray <rraayy246@gmail.com>
2021-04-08 17:47:56 +0800

    initramfs

--- jb/bf.fish
+++ jb/bf.fish
@@ -6,5 +6,5 @@ set root (string split ' ' (string match '* /' (df)))
 sudo umount /mnt
 sudo mount $root[1] /mnt
 sudo btrfs subvolume delete -c /mnt/b
-sudo btrfs subvolume snapshot /mnt/a /mnt/b
+sudo btrfs subvolume snapshot /mnt/a /mnt/b; and sudo mkinitcpio -g /boot/initramfs-linux-b.img
 sudo umount /mnt

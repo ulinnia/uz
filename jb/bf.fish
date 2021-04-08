04d4b45d682b49fb20285f30018a67f1070b5026
ray <rraayy246@gmail.com>
2021-04-08 22:56:45 +0800

    cron

--- jb/bf.fish
+++ jb/bf.fish
@@ -6,5 +6,7 @@ set root (string split ' ' (string match '* /' (df)))
 sudo umount /mnt
 sudo mount $root[1] /mnt
 sudo btrfs subvolume delete -c /mnt/b
-sudo btrfs subvolume snapshot /mnt/a /mnt/b; and sudo mkinitcpio -g /boot/initramfs-linux-b.img
+sudo btrfs subvolume snapshot /mnt/a /mnt/b;
+and sudo mkinitcpio -g /boot/initramfs-linux-b.img;
+or exit 1
 sudo umount /mnt

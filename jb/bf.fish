4e36293040dfcb1d318575df3ac4c80483a70b1b
ray <rraayy246@gmail.com>
2021-05-05 19:49:49 +0800

    exit

--- jb/bf.fish
+++ jb/bf.fish
@@ -6,21 +6,21 @@ set root (string split ' ' (string match '* /' (df)))
 if df | grep -q ' /mnt'
     sudo umount /mnt; or begin
         echo 卸载 /mnt 失败
-        exit 1
+        exit
     end
 end
 sudo mount $root[1] /mnt; or begin
     echo 挂载根目录失败 $root[1]
-    exit 2
+    exit
 end
 sudo btrfs subvolume delete -c /mnt/b; or begin
     echo 删除子卷失败
-    exit 3
+    exit
 end
 sudo btrfs subvolume snapshot /mnt/a /mnt/b;
 and sudo mkinitcpio -g /boot/initramfs-linux-b.img; or begin
     echo 创建备份失败
-    exit 4
+    exit
 end
 sudo umount /mnt
 echo 备份成功

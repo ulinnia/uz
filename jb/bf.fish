22e144302eb66c7df03964d17018579d244f8c37
ray <rraayy246@gmail.com>
2021-05-05 19:44:44 +0800

    umount

--- jb/bf.fish
+++ jb/bf.fish
@@ -4,20 +4,23 @@
 set root (string split ' ' (string match '* /' (df)))
 
 if df | grep -q ' /mnt'
-    sudo umount /mnt
+    sudo umount /mnt; or begin
+        echo 卸载 /mnt 失败
+        exit 1
+    end
 end
 sudo mount $root[1] /mnt; or begin
     echo 挂载根目录失败 $root[1]
-    exit 1
+    exit 2
 end
 sudo btrfs subvolume delete -c /mnt/b; or begin
     echo 删除子卷失败
-    exit 2
+    exit 3
 end
 sudo btrfs subvolume snapshot /mnt/a /mnt/b;
 and sudo mkinitcpio -g /boot/initramfs-linux-b.img; or begin
     echo 创建备份失败
-    exit 3
+    exit 4
 end
 sudo umount /mnt
 echo 备份成功

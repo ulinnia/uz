1045c66ce4d309662cca8708584b466fd6427494
ray <rraayy246@gmail.com>
2021-07-27 16:33:44 +0800

    wg 交互

--- jb/wg.fish
+++ jb/wg.fish
@@ -9,18 +9,24 @@ cd $config_dir || begin
 end
 
 function wg0_av
+    set i (sudo wg)
+    if test -n "$i"
+        sudo wg-quick down wg0
+    end
+    rm -rf $config_dir/*
+
     # 打开流量转发
     if not sudo grep -q 'ip_forward' /etc/sysctl.d/99-sysctl.conf
         echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.d/99-sysctl.conf
         sudo sysctl (bat /etc/sysctl.d/99-sysctl.conf | sed 's/ //g')
     end
 
-    # 生成8对密钥，分别用作1服务器和7客户端使用
-    for i in (seq 8)
-        wg genkey | tee pri"$i" | wg pubkey >pub"$i"
-        # 设置密钥访问权限
-        chmod 600 pri"$i"
-    end
+    # 生成密钥，分别用作服务器和客户端使用
+    wg genkey | tee pri1 | wg pubkey >pub1
+    wg genkey | tee pri2 | wg pubkey >pub2
+    # 设置密钥访问权限
+    chmod 600 pri1
+    chmod 600 pri2
 
     # 随机数
     function rand -a min -a max
@@ -41,35 +47,31 @@ Address = 10.10.10.1
 ListenPort = "$port"
 PostUp   = nft add rule inet filter input udp dport "$port" accept; nft add rule inet filter forward iifname wg0 accept; nft add rule inet filter forward oifname wg0 accept; nft add rule inet nat postrouting oifname "$interface" masquerade
 PostDown = nft flush ruleset; nft -f /etc/nftables.conf
+
+[Peer]
+PublicKey = "(cat pub2)"
+AllowedIPs = 10.10.10.2/32
 " > wg0.conf
 
-    for i in (seq 2 8)
-        echo "\
-[Peer]
-PublicKey = "(cat pub"$i")"
-AllowedIPs = 10.10.10."$i"/32
-" >> wg0.conf
-
-        # 生成客户端配置文件
-        echo "\
+    # 生成客户端配置文件
+    echo "\
 [Interface]
-PrivateKey = "(cat pri"$i")"
-Address = 10.10.10."$i"
+PrivateKey = "(cat pri2)"
+Address = 10.10.10.2
 DNS = 1.1.1.1
 
 [Peer]
 PublicKey = "(cat pub1)"
 Endpoint = "$ip":"$port"
 AllowedIPs = 0.0.0.0/0
-" > client"$i".conf
-
-    end
+" > client2.conf
 
     # 复制配置文件并启动
     sudo cp wg0.conf /etc/wireguard/ || begin
         echo 复制失败,请检查/etc/wireguard目录或wg0.conf是否存在
         exit
     end
+    rm wg0.conf
     sudo wg-quick up wg0 || begin
         echo 启动wireguard失败，请检查/etc/wireguard/wg0.conf是否存在错误
         exit
@@ -79,13 +81,55 @@ AllowedIPs = 0.0.0.0/0
     sudo systemctl enable wg-quick@wg0
 
     echo "安装完成！"
+    cli_av
+end
+
+function cli_av
+    while true
+        set cli (sudo cat /etc/wireguard/wg0.conf | grep -oP '(?<=\.)\d+(?=\/)')
+        echo 已存在成员：
+        echo $cli
+
+        echo 输入成员数字（存在则删除，不存在则创建）
+        read -p 'echo "> "' i
+        if test "$i" -ge 2 -a "$i" -le 254
+            if echo $cli | grep -qE '(^| )'$i'($| )'
+                sudo wg set wg0 peer (cat pub"$i") remove
+                sudo wg-quick save wg0
+            else
+                wg genkey | tee pri"$i" | wg pubkey >pub"$i"
+                chmod 600 pri"$i"
+                sudo wg set wg0 peer (cat pub"$i") allowed-ips 10.10.10."$i"/32
+                sudo wg-quick save wg0
+
+                set interface (ip -o -4 route show to default | awk '{print $5}')
+                set ip (ip -4 addr show $interface | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+                set port (sudo cat /etc/wireguard/wg0.conf | grep -oP '(?<=ListenPort = )\d+')
+
+                echo "\
+[Interface]
+PrivateKey = "(cat pri"$i")"
+Address = 10.10.10.2
+DNS = 1.1.1.1
+
+[Peer]
+PublicKey = "(cat pub1)"
+Endpoint = "$ip":"$port"
+AllowedIPs = 0.0.0.0/0
+" > client"$i".conf
+            end
+        else
+            break
+        end
+    end
     cli_ud
 end
 
 function cli_ud
     while true
-        read -p 'echo 输入成员数字[2-8]，按 q 退出：' i
-        if string match -qr '^[2-8]$' $i
+        echo 输入成员数字
+        read -p 'echo "> "' i
+        if test "$i" -ge 2 -a "$i" -le 254
             echo -e "\n=======请将此复制到 client"$i".conf 文件========\n"
             cat client"$i".conf
             echo -e "=============================================\n"
@@ -98,12 +142,15 @@ function cli_ud
 end
 
 echo 输入 a 安装 wg0
-echo 输入 b 查看成员配置
+echo 输入 b 增减成员
+echo 输入 c 查看成员配置
 read -p 'echo "> "' i
 switch $i
 case a
     wg0_av
 case b
+    cli_av
+case c
     cli_ud
 end
 

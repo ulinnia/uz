b4ed514bd3d29b75213f771419194df29e304d58
ray <rraayy246@gmail.com>
2021-09-06 18:23:54 +0800

    www 删除

--- jb/www.fish
+++
@@ -1,7 +0,0 @@
-
-
-# 增加 UDP 接收缓冲区大小
-if not sudo grep -q 'net.core.rmem_max' /etc/sysctl.d/99-sysctl.conf
-    echo 'net.core.rmem_max = 2500000' | sudo tee -a /etc/sysctl.d/99-sysctl.conf
-    sudo sysctl (bat /etc/sysctl.d/99-sysctl.conf | sed 's/ //g')
-end

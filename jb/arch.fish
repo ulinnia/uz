75f2d8bfd17c75490beb308e8e2d1f4f5e00d117
ray <rraayy246@gmail.com>
2021-09-27 22:31:41 +0800

    arch.fish

--- jb/arch.fish
+++ jb/arch.fish
@@ -38,6 +38,8 @@ function system_check
         set bios_type 'bios'
         set base_pkg $base_pkg 'dosfstools'
     end
+    # 根分区
+    set root_part (df | awk '$6=="/" {print $1}')
     # cpu 型号
     if lscpu | grep AuthenticAMD &>/dev/null
         set cpu_vendor 'amd'
@@ -64,12 +66,36 @@ end
 
 # 本地化
 function local_set
+    # 安装基本软件包
+    pacman -S $base_pkg
+    # 设置时区
     ln -sf /usr/share/zoneinfo/$area /etc/localtime
+    # 同步时钟
     hwclock --systohc
+    # 语言信息
     sed -i '/\(en_US\|zh_CN\).UTF-8/s/#//' /etc/locale.gen
     locale-gen
     echo LANG=en_US.UTF-8 > /etc/locale.conf
+    # 主机表
     echo -e '127.0.0.1\tlocalhost\n::1\t\tlocalhost\n127.0.1.1\t'$hostname'.localdomain '$hostname | sudo tee -a /etc/hosts
+
+    # 设定根密码
+    R 'enter your root passwd: '
+    passwd
+
+    # 安装引导程序
+    if test $bios_type == 'uefi'
+        grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=grub
+    else
+        if echo $root_part | grep 'nvme'
+            set grub_part (echo $i | sed 's/p[0-9]$//')
+        else
+            set grub_part (echo $i | sed 's/[0-9]$//')
+        end
+        grub-install --target=i386-pc $grub_part
+    end
+    # 生成 grub 主配置文件
+    grub-mkconfig -o /boot/grub/grub.cfg
 end
 
 # 主程序

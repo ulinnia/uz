f3ba0278731f2e91e94bdc4f38f99443401fde92
ray <rraayy246@gmail.com>
2021-12-24 12:35:48 +0800

    tcp_bbr

---
+++ jb/tcp_bbr.fish
@@ -0,0 +1,8 @@
+# 为内核加载 bbr 模块
+echo 'tcp_bbr' | sudo tee /etc/modules-load.d/modules.conf
+
+# 将默认的拥塞算法设置为 bbr
+echo 'net.core.default_qdisc=fq' | sudo tee /etc/sysctl.d/bbr.conf
+echo 'net.ipv4.tcp_congestion_control=bbr' | sudo tee -a /etc/sysctl.d/bbr.conf
+
+echo 'please reboot.'

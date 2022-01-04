b5cf1fcf47f95abd74bb7081a00eb543abb08557
ray <rraayy246@gmail.com>
2022-01-04 22:50:43 +0800

    matrix synapse

--- jb/matrix.fish
+++ jb/matrix.fish
@@ -1,2 +1,45 @@
 sudo pacman -S matrix-synapse postgresql python-psycopg2
 
+# 初始化数据库
+sudo -u postgres initdb --locale=en_US.UTF-8 -E UTF8 -D /var/lib/postgres/data
+sudo systemctl enable --now postgresql
+
+echo '设定数据库的用户口令：'
+sudo -u postgres createuser --pwprompt synapse
+sudo -u postgres createdb --encoding=UTF8 --locale=C --template=template0 --owner=synapse synapse
+
+# 生成配置文件
+read -p 'echo 输入服务器的域名：' domain_name
+
+sudo -u synapse python -m synapse.app.homeserver \
+    --server-name $domain_name \
+    --config-path /etc/synapse/homeserver.yaml \
+    --generate-config \
+    --report-stats=yes
+
+echo '
+修改 /etc/synapse/homeserver.yaml 配置
+
+找到：
+database:
+  name: sqlite3
+  args:
+    database: /opt/synapse/homeserver.db
+
+修改为：
+database:
+  name: psycopg2
+  args:
+    user: synapse
+    password: 你刚才设定的密码
+    database: synapse
+    host: localhost
+    cp_min: 5
+    cp_max: 10
+
+要启用注册，可以设置 enable_registration 到 True
+
+最后设定自启动：
+sudo systemctl enable --now synapse
+'
+
